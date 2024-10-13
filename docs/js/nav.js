97691e463bc66c9b027238a6f5a0ea79dc9283f3
maksimilianimys <maswety8@example.com>
2024-10-13 13:30:24 +0300

    Lab-8

---
+++ docs/js/nav.js
@@ -0,0 +1,1 @@
+(()=>{const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),o=document.querySelector("body");e.addEventListener("click",(()=>{const s="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!s),t.classList.toggle("is-open"),o.classList.toggle("no-scroll")}))})();
