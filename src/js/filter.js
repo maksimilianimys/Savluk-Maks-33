19df0f676c36b61dc605a4e7532434724d73a79f
maksimilianimys <maswety8@example.com>
2024-10-14 21:23:26 +0300

    Fix lab-8

--- src/js/filter.js
+++ src/js/filter.js
@@ -2,16 +2,16 @@ const filterButton = document.querySelectorAll(".button-row__filter");
 const filterableCard = document.querySelectorAll(".portfolio-section-list-projects-element");
 
 const filterCard = (e) => {
-  document.querySelector(".active").classList.remove("active");
-  e.target.classList.add("active");
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
 
-  filterableCard.forEach((card) => {
-    card.classList.add("hide");
+    filterableCard.forEach((card) => {
+        card.classList.add("hide");
 
-    if (card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all") {
-      card.classList.remove("hide");
-    }
-  });
+        if (card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all") {
+            card.classList.remove("hide");
+        }
+    });
 };
 
 filterButton.forEach((button) => button.addEventListener("click", filterCard));
--- src/js/mobile-nav.js
+++ src/js/mobile-nav.js
@@ -3,10 +3,10 @@
     const mobileMenuRef = document.querySelector('[data-menu]');
     const body = document.querySelector('body');
     menuBtnRef.addEventListener('click', () => {
-      const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-      menuBtnRef.classList.toggle('is-open');
-      menuBtnRef.setAttribute('aria-expanded', !expanded);
-      mobileMenuRef.classList.toggle('is-open');
-      body.classList.toggle('no-scroll');
+        const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+        menuBtnRef.classList.toggle('is-open');
+        menuBtnRef.setAttribute('aria-expanded', !expanded);
+        mobileMenuRef.classList.toggle('is-open');
+        body.classList.toggle('no-scroll');
     });
-  })();
+})();
